76834f636f9a8e05a4b138e6300053cbe5a0656b
Margui <margui.berjon@gmail.com>
2020-05-27 17:10:51 +0200

    feature: call api done

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ form.addEventListener('submit', callApiAjax);
 
 function callApiAjax(ev) {
   ev.preventDefault();
-  console.log('click');
+  const origin = document.querySelector('.js-origin');
+  const originSelected = origin.options[origin.selectedIndex].value;
+  const gender = document.querySelector('.js-gender');
+  const genderSelected = gender.options[gender.selectedIndex].value;
+  const number = document.querySelector('.js-number').value;
+  let ENDPOINT = '';
 
+  ENDPOINT += 'https://randomuser.me/api/?';
+  if (originSelected !== '') {
+    ENDPOINT += `nat=${originSelected}&`;
+  }
+  if (genderSelected !== '') {
+    ENDPOINT += `gender=${genderSelected}&`;
+  }
+
+  if (number !== '') {
+    ENDPOINT += `results=${number}`;
+  }
+  console.log(ENDPOINT);
 }
