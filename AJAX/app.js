d04d28f5e2c16db8f7fbe9c16acd2f8dab579cfe
Margui <margui.berjon@gmail.com>
2020-05-28 19:02:55 +0200

    feature: changed from AJAX to fetch

--- AJAX/app.js
+++ AJAX/app.js
@@ -22,5 +22,28 @@ function callApiAjax(ev) {
   if (number !== '') {
     ENDPOINT += `results=${number}`;
   }
-  console.log(ENDPOINT);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', ENDPOINT, true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const names = JSON.parse(this.responseText).results;
+      console.log(names);
+
+      let htmlNames = '<h2> Nombres generados</h2>';
+      htmlNames += '<ul>'
+
+      names.forEach(function (name) {
+        htmlNames += `
+        <li>${name.name.first}</li>
+        `
+
+      });
+      htmlNames += '</ul>';
+      document.getElementById('js-result').innerHTML = htmlNames;
+
+    }
+  }
+  xhr.send();
 }
---
+++ Fetch API/app.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('.js-form');
+
+form.addEventListener('submit', callApiAjax);
+
+function callApiAjax(ev) {
+  ev.preventDefault();
+  const origin = document.querySelector('.js-origin');
+  const originSelected = origin.options[origin.selectedIndex].value;
+  const gender = document.querySelector('.js-gender');
+  const genderSelected = gender.options[gender.selectedIndex].value;
+  const number = document.querySelector('.js-number').value;
+  let ENDPOINT = '';
+
+  ENDPOINT += 'https://randomuser.me/api/?';
+  if (originSelected !== '') {
+    ENDPOINT += `nat=${originSelected}&`;
+  }
+  if (genderSelected !== '') {
+    ENDPOINT += `gender=${genderSelected}&`;
+  }
+
+  if (number !== '') {
+    ENDPOINT += `results=${number}`;
+  }
+
+  fetch(ENDPOINT)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (names) {
+      let results = names.results
+
+      let htmlNames = '<h2> Nombres generados</h2>';
+      htmlNames += '<ul>'
+
+      results.forEach(function (result) {
+        htmlNames += `
+        <li>${result.name.first}</li>
+        `;
+
+      })
+      htmlNames += '</ul>';
+      document.getElementById('js-result').innerHTML = htmlNames;
+
+    }).catch(function (error) {
+      console.log(error);
+
+    })
+
+}
